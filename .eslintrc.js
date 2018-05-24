12484418fbe01c93b865b811f4311e7c10ca53e2
Zen Pandawan <38893681+Zen-Pandawan@users.noreply.github.com>
2018-05-23 21:34:00 -0400

    Project Setup
    add linters, eslint plugin, custom rules in eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  plugins: [
+    'ember'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:ember-best-practices/recommended'
+  ],
+  env: {
+    browser: true
+  },
+  rules: {
+    indent: [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
+    ],
+    'no-loop-func': 'error',
+    'no-new-func': 'error',
+    'no-new-wrappers': 'error',
+    'no-with': 'error',
+    'vars-on-top': 'error'
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        es6: true,
+        node: true
+      }
+    }
+  ]
+};
